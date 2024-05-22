8f7a61561b5736e9b5dc42127247bba293156155
Kent Kolze <kent@pluralenergy.co>
2024-05-22 07:40:13 -0500

    redeploy files, switch deployment to store history like in plural protocol

--- scripts/deployFactory.sh
+++ scripts/deployFactory.sh
@@ -5,6 +5,20 @@ USE_ENV_FILE=${1:-.env}
 
 source $USE_ENV_FILE
 
+# chain ID based on RPC_URL
+chain_id=$(cast chain-id --rpc-url "$RPC_URL")
+
+# Get the current timestamp
+timestamp=$(date +%s)
+
+# Create the deployment directory
+deployment_dir="deployments/$chain_id/$timestamp"
+mkdir -p "chain/$deployment_dir"
+
+# Set the log file path
+log_file="$deployment_dir/deploy.log"
+
+
 # Copy the root .env underneath chain so we dont have to maintain two copies
 TEMP=$PWD/chain/.env
 cp $USE_ENV_FILE $TEMP
@@ -12,4 +26,9 @@ trap "rm $TEMP" EXIT
 
 set -x
 cd chain
-forge script script/CapTableFactory.s.sol:DeployCapTableFactoryDeployLocalScript --broadcast --fork-url $RPC_URL
+forge script script/CapTableFactory.s.sol:DeployCapTableFactoryDeployLocalScript --broadcast --fork-url $RPC_URL --ast --out $deployment_dir -vvvv > $log_file 2>&1
+
+rm -rf out
+cp -r $deployment_dir out
+echo "✅ Finished TAP Deployment! See out/deploy.log for details"
+echo "TAP_TMP=chain/${deployment_dir}"
